b6645a445d60dff5d78b444bf2daceafdc2fd441
Sammy Nave <sammy.nave@gmail.com>
2018-06-25 10:38:57 -0400

    fetch emails

---
+++ app/controllers/emails.js
@@ -0,0 +1,9 @@
+import Controller from '@ember/controller';
+import { or, readOnly } from '@ember/object/computed';
+
+export default Controller.extend({
+  emails: or('model.{currentTask,previousTask}.value'),
+  isLoading: readOnly('model.currentTask.isRunning'),
+  hasError: readOnly('model.currentTask.isError'),
+  error: readOnly('model.currentTask.error')
+});
